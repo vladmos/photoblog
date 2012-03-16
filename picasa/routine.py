28a39bb89a4bd00483777c1ba2ea0de2df9251d9
Vladimir Moskva <m@fulc.ru>
2012-03-16 20:23:09 +0400

    Disabling token on unsuccessful authorization attempt

--- picasa/routine.py
+++ picasa/routine.py
@@ -1,8 +1,21 @@
 import gdata.auth
 import gdata.photos.service
+from gdata.photos.service import GooglePhotosException
+
 from django.contrib.auth.models import User
 
 from models import PicasaAlbum
+
+def _populate_album(user, album):
+    try:
+        picasa_album = PicasaAlbum.objects.get(picasa_id=album.gphoto_id.text)
+    except PicasaAlbum.DoesNotExist:
+        picasa_album = PicasaAlbum(picasa_id=album.gphoto_id.text)
+
+    picasa_album.user = user
+    picasa_album.name = album.name.text
+    picasa_album.is_public = (album.access.text == 'public')
+    picasa_album.save()
 
 def fetch_albums():
     for user in User.objects.filter(profile__oauth_token__isnull=False):
@@ -15,14 +28,12 @@ def fetch_albums():
         picasa_service = gdata.photos.service.PhotosService()
         picasa_service.SetOAuthToken(token)
 
-        albums = picasa_service.GetUserFeed()
+        try:
+            albums = picasa_service.GetUserFeed()
 
-        for album in albums.entry:
-            try:
-                picasa_album = PicasaAlbum.objects.get(picasa_id=album.gphoto_id.text)
-            except PicasaAlbum.DoesNotExist:
-                picasa_album = PicasaAlbum(picasa_id=album.gphoto_id.text)
-            picasa_album.user = user
-            picasa_album.name = album.name.text
-            picasa_album.is_public = (album.access.text == 'public')
-            picasa_album.save()
+            for album in albums.entry:
+                _populate_album(user, album)
+
+        except GooglePhotosException:
+            user.profile.is_valid_token = False
+            user.profile.save()
