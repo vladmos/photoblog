ab4552829438b2787f5d4c3c29a866232860b009
Vladimir Moskva <m@fulc.ru>
2012-03-16 22:18:26 +0400

    Removing obsolete picasa albums and photos from the local base

--- picasa/models.py
+++ picasa/models.py
@@ -4,13 +4,20 @@ from django.contrib.auth.models import User
 class PicasaAlbum(models.Model):
     user = models.ForeignKey(User, related_name='albums')
     name = models.CharField(max_length=255)
-    picasa_id = models.CharField(max_length=20, unique=True)
+    picasa_id = models.CharField(max_length=20)
     is_public = models.BooleanField()
+
+    class Meta:
+        unique_together = ('picasa_id', 'user')
+
 
 class PicasaPhoto(models.Model):
     album = models.ForeignKey('PicasaAlbum', related_name='photos')
-    picasa_id = models.CharField(max_length=20, unique=True)
+    picasa_id = models.CharField(max_length=20)
     page_url = models.URLField()
     photo_url = models.URLField()
     thumbnail_url = models.URLField()
     description = models.TextField(null=True)
+
+    class Meta:
+        unique_together = ('picasa_id', 'album')
--- picasa/routine.py
+++ picasa/routine.py
@@ -7,10 +7,14 @@ from django.contrib.auth.models import User
 from models import PicasaAlbum, PicasaPhoto
 
 def _update_photos(picasa_album, photos):
+    picasa_photos = PicasaPhoto.objects.filter(album=picasa_album)
+    obsolete_picasa_photos_id = {p.id for p in picasa_photos}
+
     for photo in photos.entry:
 
         try:
             picasa_photo = PicasaPhoto.objects.get(picasa_id=photo.gphoto_id.text)
+            obsolete_picasa_photos_id.remove(picasa_photo.id)
         except PicasaPhoto.DoesNotExist:
             picasa_photo = PicasaPhoto(picasa_id=photo.gphoto_id.text)
 
@@ -21,11 +25,17 @@ def _update_photos(picasa_album, photos):
         picasa_photo.description = photo.summary.text
         picasa_photo.save()
 
+    PicasaPhoto.objects.filter(id__in=obsolete_picasa_photos_id).delete()
+
 def _update_albums(picasa_service, user, albums):
+    picasa_albums = PicasaAlbum.objects.filter(user=user)
+    obsolete_picasa_albums_id = {a.id for a in picasa_albums}
+
     for album in albums.entry:
 
         try:
             picasa_album = PicasaAlbum.objects.get(picasa_id=album.gphoto_id.text)
+            obsolete_picasa_albums_id.remove(picasa_album.id)
         except PicasaAlbum.DoesNotExist:
             picasa_album = PicasaAlbum(picasa_id=album.gphoto_id.text)
 
@@ -37,6 +47,8 @@ def _update_albums(picasa_service, user, albums):
         photos_uri = album.GetPhotosUri()
         photos = picasa_service.GetFeed(photos_uri)
         _update_photos(picasa_album, photos)
+
+    PicasaAlbum.objects.filter(id__in=obsolete_picasa_albums_id).delete()
 
 def fetch_albums():
     for user in User.objects.filter(profile__oauth_token__isnull=False):
