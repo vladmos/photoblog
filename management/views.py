f6f357932debc2c73a1ab192b59795fdd6c3ca01
Vladimir Moskva <m@fulc.ru>
2012-03-17 04:15:02 +0400

    Removed redundant variables

--- management/views.py
+++ management/views.py
@@ -7,29 +7,21 @@ from picasa.models import PicasaAlbum
 
 @login_required
 def index(request):
-    my_albums = PicasaAlbum.objects.filter(user=request.user)
     return response(request, 'index.html', {
-        'photoalbums': my_albums,
         'google_token_management_url': settings.GOOGLE_TOKEN_MANAGEMENT_URL,
     })
 
 @login_required
 def photoalbum(request, photoalbum_id):
-    my_albums = PicasaAlbum.objects.filter(user=request.user)
     album = get_object_or_404(PicasaAlbum, id=photoalbum_id)
 
     if album.user.id != request.user.id:
         return redirect('management:photoalbums')
 
     return response(request, 'photoalbum.html', {
-        'photoalbums': my_albums,
         'photoalbum': album,
     })
 
 @login_required
 def photoalbums(request):
-    my_albums = PicasaAlbum.objects.filter(user=request.user)
-
-    return response(request, 'photoalbums.html', {
-        'photoalbums': my_albums,
-        })
+    return response(request, 'photoalbums.html')
