e4aa1e3dc5376fe99237d7384b58c1b37bfa0d55
Vladimir Moskva <vladmos@yandex-team.ru>
2012-03-20 11:45:45 +0400

    Switched default config to sqlite database

--- settings.py
+++ settings.py
@@ -14,12 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'photoalbum',                      # Or path to database file if using sqlite3.
-        'USER': 'photoalbum',                      # Not used with sqlite3.
-        'PASSWORD': 'photoalbum',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(_project_root, 'db.sql'),
     }
 }
 
